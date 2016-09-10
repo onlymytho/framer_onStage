883a0b39f4ef0d0dad1d4205e0164d9098844e4a
SangHyuk Henry Jung <onlymytho@gmail.com>
2016-09-11 02:38:11 +0900

    bunch updated

--- Entry_stage/15min_thread.framer/framer/framer.generated.js
+++ Entry_stage/15min_thread.framer/framer/framer.generated.js
@@ -7,7 +7,7 @@ if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","se
 }
 if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
 }
-window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"15min_thread_.framer"};
+window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"15min_thread.framer"};
 
 Framer.Device = new Framer.DeviceView();
 Framer.Device.setupContext();
